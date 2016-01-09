86f1697b6b6df2af9c915066437268a20423c350
Dr. Heinrich Hördegen <hoerdegen@energiefluss.info>
2016-01-09 16:03:19 +0100

    initGame

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,26 @@ module Main where
 import Prelude
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
+import Control.Monad.Eff.Random
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Data.Array
+import Data.Traversable
+import Data.Maybe
+import Data.Foldable
+
+type Line = Array Number
+
+type Game = Array Line
+
+-- [l] --  sequence <<< (map (const randomBool) l)
+
+initGame :: forall e. Int -> Eff (random :: RANDOM | e) Game
+initGame n = do
+  xs <- sequence $ map (const random) l
+  return [xs]
+  where l = (0 .. n)
+
+
+main :: forall e. Eff (console :: CONSOLE, random :: RANDOM | e) Unit
 main = do
   log "Hello sailor!"
