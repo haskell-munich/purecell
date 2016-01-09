2ab2dfc91ba5ac2b6c67a5c2d341765f8061e53d
Markus Liedl <m.liedl@gmx.de>
2016-01-09 18:13:15 +0100

    rule 110 and 124

--- src/Main.purs
+++ src/Main.purs
@@ -9,6 +9,7 @@ import Data.Array
 import Data.Traversable
 import Data.Maybe
 import Data.Foldable
+import Data.String (fromCharArray)
 
 type Line = Array Int
 
@@ -21,8 +22,50 @@ initGame n = do
   xs <- sequence $ map (const (map f random)) l
   return [xs]
   where l = (0 .. n)
-        f x = if (x < 0.5) then 0 else 1
+        f x = if (x < 0.9) then 0 else 1
+
+rule110 [0, 0, 0] = 0
+rule110 [0, 0, 1] = 1
+rule110 [0, 1, 0] = 1
+rule110 [0, 1, 1] = 1
+rule110 [1, 0, 0] = 0
+rule110 [1, 0, 1] = 1
+rule110 [1, 1, 0] = 1
+rule110 [1, 1, 1] = 0
+
+rule124 [0, 0, 0] = 0
+rule124 [0, 0, 1] = 1
+rule124 [0, 1, 0] = 1
+rule124 [0, 1, 1] = 1
+rule124 [1, 0, 0] = 1
+rule124 [1, 0, 1] = 1
+rule124 [1, 1, 0] = 1
+rule124 [1, 1, 1] = 0
+
+rule = rule124
+
+triplesFromArray ary0 = do
+  i <- 0 .. (length ary - 3)
+  return (slice i (i+3) ary)
+  where ary = cons 0 $ snoc ary0 0
+
+nextState ary = map rule $ triplesFromArray ary
+
+developGame n [state] = develop n state
+
+develop 0 state = [state]
+develop n state = state : develop (n-1) (nextState state)
+
+nice 0 = ' '
+nice 1 = 'x'
+
+printNice xs = print $ fromCharArray $ map nice xs
+
+test = do
+  sequence $ map printNice $
+    Main.develop 150 (snoc (replicate 150 0) 1)
+  return unit
 
 main :: forall e. Eff (console :: CONSOLE, random :: RANDOM | e) Unit
 main = do
-  log "Hello sailor!"
+  test
